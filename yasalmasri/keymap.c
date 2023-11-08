00d78621915d8fc5b9dbd922df25c089cd2edc8d
Yaser Almasri <yaser@masys.mx>
2023-11-08 08:49:31 -0600

    colemak

--- yasalmasri/keymap.c
+++ yasalmasri/keymap.c
@@ -16,16 +16,18 @@
 
 #include QMK_KEYBOARD_H
 
-enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST, _NUMPAD };
+enum planck_layers { _QWERTY, _COLEMAK, _LOWER, _RAISE, _ADJUST, _NUMPAD };
 
-enum layers {
-  QWERTY = SAFE_RANGE,
-  BACKLIT,
-  EXT_PLV
-};
+// enum layers {
+//   QWERTY = SAFE_RANGE,
+//   BACKLIT,
+//   EXT_PLV
+// };
 
 enum custom_keycodes {
   MS_SAFE = SAFE_RANGE,
+  QWERTY,
+  COLEMAK,
   MS_SPCEQL,
   MS_LEFT_BOTTOM,
   MS_RIGHT_BOTTOM,
@@ -58,6 +60,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,  KC_K,    KC_L,    KC_QUOT, KC_ENT ,
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN,
     NUMPAD,         KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, /**/ KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+  
+/* Colemak
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |  '   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_COLEMAK] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT ,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    NUMPAD,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -110,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, /**/ RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, /**/ AG_SWAP, _______, _______, _______, _______, _______,
+    _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, /**/ AG_SWAP, QWERTY,  COLEMAK, _______, _______, _______,
     _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   /**/ MI_OFF,  _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______
 ),
@@ -127,10 +147,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = LAYOUT_planck_grid(
-    KC_TRNS, MS_TMUX_SV,     MS_TMUX_SH,      KC_NO,           KC_NO, KC_NO, /**/ KC_NO, KC_P7, KC_P8,   KC_P9,   KC_PPLS, KC_TRNS,
-    KC_TRNS, KC_NO,          MS_TMUX_SESSION, MS_TMUX_DETTACH, KC_NO, KC_NO, /**/ KC_NO, KC_P4, KC_P5,   KC_P6,   KC_PMNS, KC_TRNS,
-    KC_NO,   MS_LEFT_BOTTOM, MS_RIGHT_BOTTOM, KC_NO,           KC_NO, KC_NO, /**/ KC_NO, KC_P1, KC_P2,   KC_P3,   KC_PAST, KC_PEQL,
-    KC_TRNS, KC_NO,          KC_NO,           KC_NO,           KC_NO, KC_NO, /**/ KC_NO, KC_P0, KC_PDOT, KC_PDOT, KC_PSLS, KC_PENT
+    KC_TRNS, MS_TMUX_SV,     MS_TMUX_SH,      KC_NO,           KC_NO, KC_NO,   /**/ KC_NO,   KC_P7, KC_P8,   KC_P9,   KC_PPLS, KC_TRNS,
+    KC_TRNS, KC_NO,          MS_TMUX_SESSION, MS_TMUX_DETTACH, KC_NO, KC_NO,   /**/ KC_NO,   KC_P4, KC_P5,   KC_P6,   KC_PMNS, KC_TRNS,
+    KC_NO,   MS_LEFT_BOTTOM, MS_RIGHT_BOTTOM, KC_NO,           KC_NO, KC_NO,   /**/ KC_NO,   KC_P1, KC_P2,   KC_P3,   KC_PAST, KC_PEQL,
+    KC_TRNS, KC_NO,          KC_NO,           KC_NO,           KC_NO, _______, /**/ _______, KC_P0, KC_PDOT, KC_PDOT, KC_PSLS, KC_PENT
 )
 
 };
@@ -142,22 +162,20 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    // case QWERTY:
-    //   if (record->event.pressed) {
-    //     print("mode just switched to qwerty and this is a huge string\n");
-    //     set_single_persistent_default_layer(_QWERTY);
-    //   }
-    //   return false;
-    //   break;
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
 
-    // case BACKLIT:
-    //   if (record->event.pressed) {
-    //     register_code(KC_RSFT);
-    //   } else {
-    //     unregister_code(KC_RSFT);
-    //   }
-    //   return false;
-    //   break;
+    case COLEMAK:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
 
     case MS_SPCEQL:
       if (record->event.pressed) {
