aa4c115610f10c2a433bea53afb246a3cd00e626
Yaser Almasri <yaser@masys.mx>
2023-11-22 23:37:21 -0600

    layer 2

--- yasalmasri/keymap.c
+++ yasalmasri/keymap.c
@@ -47,7 +47,7 @@ enum custom_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define NUMPAD MO(_NUMPAD)
-#define LAYER2 LT(_LAYER2, KC_SCLN)
+#define LAYER2 LT(_LAYER2, KC_LEFT)
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,         KC_Q,    KC_W,   KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_T(KC_ESC), KC_A,    KC_S,   KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,   KC_K,    KC_L,    KC_QUOT, KC_ENT,
-    KC_LSFT,        KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, LAYER2,
-    KC_LCTL,        KC_LALT, NUMPAD, KC_LGUI, LOWER, KC_SPC, /**/ KC_SPC, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LSFT,        KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN,
+    KC_LCTL,        KC_LALT, NUMPAD, KC_LGUI, LOWER, KC_SPC, /**/ KC_SPC, RAISE,  LAYER2,  KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
@@ -157,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUMPAD] = LAYOUT_planck_grid(
     KC_TRNS, MS_TMUX_SV,     MS_TMUX_SH,      KC_NO,           KC_NO, KC_NO,   /**/ KC_NO,   KC_P7, KC_P8,   KC_P9, KC_PPLS, KC_TRNS,
     KC_TRNS, KC_NO,          MS_TMUX_SESSION, MS_TMUX_DETTACH, KC_NO, KC_NO,   /**/ KC_PAST, KC_P4, KC_P5,   KC_P6, KC_PMNS, KC_TRNS,
-    KC_NO,   WN_LEFT_BOTTOM, WN_BOTTOM,       WN_RIGHT_BOTTOM, KC_NO, KC_NO,   /**/ KC_PSLS, KC_P1, KC_P2,   KC_P3, KC_NO,   KC_NO,
+    KC_TRNS, WN_LEFT_BOTTOM, WN_BOTTOM,       WN_RIGHT_BOTTOM, KC_NO, KC_NO,   /**/ KC_PSLS, KC_P1, KC_P2,   KC_P3, KC_NO,   KC_NO,
     KC_TRNS, KC_NO,          KC_NO,           KC_NO,           KC_NO, _______, /**/ _______, KC_P0, KC_PDOT, KC_NO, KC_NO,   KC_NO
 ),
 
@@ -173,10 +173,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LAYER2] = LAYOUT_planck_grid(
-    KC_TRNS, WN_LEFT_TOP,    WN_TOP,    WN_RIGHT_TOP,    KC_NO, KC_NO,   /**/ KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-    KC_TRNS, KC_NO,          WN_CENTER, KC_NO,           KC_NO, KC_NO,   /**/ KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-    KC_NO,   WN_LEFT_BOTTOM, WN_BOTTOM, WN_RIGHT_BOTTOM, KC_NO, KC_NO,   /**/ KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO,   KC_NO,          KC_NO,     KC_NO,           KC_NO, _______, /**/ _______, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   /**/ KC_NO,   KC_NO, WN_LEFT_TOP,    WN_TOP,    WN_RIGHT_TOP,    KC_TRNS,
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   /**/ KC_NO,   KC_NO, KC_NO,          WN_CENTER, KC_NO,           KC_TRNS,
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   /**/ KC_NO,   KC_NO, WN_LEFT_BOTTOM, WN_BOTTOM, WN_RIGHT_BOTTOM, KC_NO,
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, _______, /**/ _______, KC_NO, KC_NO,          KC_NO,     KC_NO,           KC_NO
 )
 
 };
