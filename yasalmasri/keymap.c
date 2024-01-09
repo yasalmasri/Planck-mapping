fd4c800b287caedb6eedb264996e503e6e44dea0
Yaser Almasri <yaser.almasri@apptegy.com>
2024-01-09 14:01:26 -0600

    move ctrl down

--- yasalmasri/keymap.c
+++ yasalmasri/keymap.c
@@ -66,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,   KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,   KC_I,    KC_O,    RCTL_T(KC_P),    KC_BSPC,
+    KC_TAB,         KC_Q,    KC_W,   KC_E,    KC_R,  KC_T,   /**/ KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,            KC_BSPC,
     LCTL_T(KC_ESC), KC_A,    KC_S,   KC_D,    KC_F,  KC_G,   /**/ KC_H,   KC_J,   KC_K,    KC_L,    RSFT_T(KC_QUOT), KC_ENT,
-    KC_LSFT,        KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,         RSFT_CAPS,
+    KC_LSFT,        KC_Z,    KC_X,   KC_C,    KC_V,  KC_B,   /**/ KC_N,   KC_M,   KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), RSFT_CAPS,
     KC_LCTL,        KC_LALT, NUMPAD, KC_LGUI, LOWER, KC_SPC, /**/ KC_SPC, RAISE,  LAYER2,  QK_LEAD, KC_UP,           KC_RGHT
 ),
 
