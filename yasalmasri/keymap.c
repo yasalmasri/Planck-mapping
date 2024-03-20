43af6252de083c5d84dd422491cbf36faf3ca33e
Yaser Almasri <yaser@masys.mx>
2024-03-19 22:37:52 -0600

    raise layer

--- yasalmasri/keymap.c
+++ yasalmasri/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_GRV,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS,
     KC_TRNS, MS_SPCEQL, KC_QUES, KC_DQUO, KC_NO,   KC_NO,   /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DQUO, _______,
     KC_TRNS, KC_MPLY,   KC_VOLD, KC_VOLU, KC_MNXT, KC_NO,   /**/ KC_COLN, KC_UNDS, KC_LBRC, KC_RBRC, KC_QUES, KC_BSLS,
     _______, _______,   _______, _______, _______, _______, /**/ _______, _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT
